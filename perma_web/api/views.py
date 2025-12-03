a44c0e498d730b10785e27e3bcd5e6dffdcf11a6
Rebecca Cremona <rcremona@law.harvard.edu>
2025-12-03 11:18:18 -0500

    Temporarily disable "public" APIs.

--- perma_web/api/views.py
+++ perma_web/api/views.py
@@ -322,11 +322,14 @@ class PublicLinkListView(BaseView):
 
     def get(self, request, format=None):
         """ List public links. """
-        queryset = Link.objects\
-            .order_by('-creation_timestamp')\
-            .select_related('capture_job')\
-            .prefetch_related('captures').discoverable()
-        return self.simple_list(request, queryset)
+
+        return HttpResponse('This route is temporarily unavilable', status=503)
+
+        # queryset = Link.objects\
+        #     .order_by('-creation_timestamp')\
+        #     .select_related('capture_job')\
+        #     .prefetch_related('captures').discoverable()
+        # return self.simple_list(request, queryset)
 
 # /public/archives/:guid
 class PublicLinkDetailView(BaseView):
@@ -335,11 +338,14 @@ class PublicLinkDetailView(BaseView):
 
     def get(self, request, guid, format=None):
         """ Get public link details. """
-        try:
-            obj = Link.objects.discoverable().get(pk=guid)
-        except Link.DoesNotExist:
-            raise Http404
-        return self.simple_get(request, obj=obj)
+
+        return HttpResponse('This route is temporarily unavilable', status=503)
+
+        # try:
+        #     obj = Link.objects.discoverable().get(pk=guid)
+        # except Link.DoesNotExist:
+        #     raise Http404
+        # return self.simple_get(request, obj=obj)
 
 
 #/public/archives/:guid/download
@@ -349,18 +355,21 @@ class PublicLinkDownloadView(BaseView):
 
     def get(self, request, guid, format=None):
         """ Download public link  """
-        try:
-            link = Link.objects.discoverable().get(pk=guid)
-        except Link.DoesNotExist:
-            raise Http404
 
-        file_format = get_download_file_format(request)
+        return HttpResponse('This route is temporarily unavilable', status=503)
 
-        if link.replacement_link_id:
-            base_url = reverse_api_view_relative('public_archives_download', kwargs={'guid': link.replacement_link_id})
-            return HttpResponseRedirect(f"{base_url}?file_format={file_format}")
+        # try:
+        #     link = Link.objects.discoverable().get(pk=guid)
+        # except Link.DoesNotExist:
+        #     raise Http404
 
-        return stream_archive(link, file_format=file_format)
+        # file_format = get_download_file_format(request)
+
+        # if link.replacement_link_id:
+        #     base_url = reverse_api_view_relative('public_archives_download', kwargs={'guid': link.replacement_link_id})
+        #     return HttpResponseRedirect(f"{base_url}?file_format={file_format}")
+
+        # return stream_archive(link, file_format=file_format)
 
 
 # /archives
