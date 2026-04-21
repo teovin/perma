c1208a2c50f18da364afd87723c4a691c5bace09
Ebru Yucesar <eyucesar@law.harvard.edu>
2026-04-21 09:53:17 -0400

    remove all, don't check for unusued import

--- perma_web/perma/models/__init__.py
+++ perma_web/perma/models/__init__.py
@@ -6,50 +6,35 @@ Two callables (get_default_archive_formats, get_empty_datetime_range) are also a
 because older migrations resolve them on perma.models (migrations 0005 and 0046).
 """
 
-from .base import (
+from .base import ( # noqa: F401
     ACTIVE_SUBSCRIPTION_STATUSES,
     FIELDS_REQUIRED_FROM_PERMA_PAYMENTS,
     link_count_in_time_period,
     most_active_org_in_time_period,
-    subscription_is_active,
+    subscription_is_active
 )
-from .folder import Folder
-from .internet_archive import (
+from .folder import Folder # noqa: F401
+from .internet_archive import ( # noqa: F401
     InternetArchiveFile,
     InternetArchiveItem,
-    get_empty_datetime_range,
+    get_empty_datetime_range
 )
-from .link import (
+from .link import ( # noqa: F401
     Capture,
     CaptureJob,
     Link,
     LinkBatch,
-    get_default_archive_formats,
+    get_default_archive_formats
 )
-from .organization import Organization
-from .registrar import Registrar, Sponsorship
-from .user import ApiKey, LinkUser, LinkUserManager, UserOrganizationAffiliation
+from .organization import Organization # noqa: F401
+from .registrar import ( # noqa: F401
+    Registrar, 
+    Sponsorship
+)
+from .user import( # noqa: F401
+    ApiKey,
+    LinkUser,
+    LinkUserManager,
+    UserOrganizationAffiliation
+)
 
-__all__ = [
-    'ACTIVE_SUBSCRIPTION_STATUSES',
-    'ApiKey',
-    'Capture',
-    'CaptureJob',
-    'FIELDS_REQUIRED_FROM_PERMA_PAYMENTS',
-    'Folder',
-    'InternetArchiveFile',
-    'InternetArchiveItem',
-    'Link',
-    'LinkBatch',
-    'LinkUser',
-    'LinkUserManager',
-    'Organization',
-    'Registrar',
-    'Sponsorship',
-    'UserOrganizationAffiliation',
-    'get_default_archive_formats',
-    'get_empty_datetime_range',
-    'link_count_in_time_period',
-    'most_active_org_in_time_period',
-    'subscription_is_active',
-]
