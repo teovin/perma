01e9abd016d380267721947be05c520bf4f0f893
Rebecca Lynn Cremona <rcremona@law.harvard.edu>
2026-05-21 15:46:19 -0400

    Add example config.

--- perma_web/perma/settings/deployments/settings_common.py
+++ perma_web/perma/settings/deployments/settings_common.py
@@ -673,6 +673,17 @@ TEMPLATE_VISIBLE_SETTINGS = (
 VIEW_SCHEMA = False
 
 # Custom staff-invited new-user emails for specific registrars.
-# Format: {registrar_id: {template_file, subject, opening, closing}}
 # Used by send_staff_invited_new_user_email.
+# Format:
+# CUSTOM_EMAILS_FOR_REGISTRAR = {
+#     1: {  # registrar id, as an int
+#         # template_file is optional, below is the default value
+#         'template_file': 'email/new_user_added_by_other_custom.txt',
+#         # below are the context values required for the default template;
+#         # supply your own keys/values as necessary if specifying your own template.
+#         'subject': 'Custom email subject line',
+#         'opening': 'Opening paragraph, before the activation link section',
+#         'closing': 'Closing paragraph, after the activation link section.',
+#     },
+# }
 CUSTOM_EMAILS_FOR_REGISTRAR = {}
