8dab87d949d4de02c58d59c7b56fbde79e32e848
Rebecca Lynn Cremona <rcremona@law.harvard.edu>
2026-06-18 16:56:49 -0400

    Lint.

--- perma_web/perma/views/user_settings.py
+++ perma_web/perma/views/user_settings.py
@@ -1,7 +1,6 @@
 import itertools
 import uuid
 
-from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
