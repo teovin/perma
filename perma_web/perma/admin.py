b1327cfcf36fe61e5c0aeba01b1f1e8569e536e1
Rebecca Lynn Cremona <rcremona@law.harvard.edu>
2026-07-22 13:39:31 -0400

    Add 'cached_paid_through' to Registrar admin, accidentally missing.

--- perma_web/perma/admin.py
+++ perma_web/perma/admin.py
@@ -478,7 +478,7 @@ class RegistrarAdmin(SimpleHistoryAdmin):
     list_filter = ('status', 'unlimited', 'nonpaying', 'cached_subscription_status', 'orgs_private_by_default')
     fieldsets = (
         (None, {'fields': ('name', 'email', 'website', 'address', 'status', 'type', 'international', 'tags', 'orgs_private_by_default', 'notes')}),
-        ("Tier", {'fields': ('nonpaying', 'base_rate', 'local_subscription_description', 'cached_subscription_started', 'cached_subscription_status', 'cached_subscription_rate', 'unlimited', 'link_limit', 'link_limit_period', 'bonus_links')}),
+        ("Tier", {'fields': ('nonpaying', 'base_rate', 'local_subscription_description', 'cached_subscription_started', 'cached_paid_through', 'cached_subscription_status', 'cached_subscription_rate', 'unlimited', 'link_limit', 'link_limit_period', 'bonus_links')}),
     )
     inlines = [
         new_class("OrganizationInline", admin.TabularInline, model=Organization,
