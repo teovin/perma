0bd2d6aa33058b79425bb626a967c51bb1ba6c57
Rebecca Lynn Cremona <rcremona@law.harvard.edu>
2026-08-04 16:46:51 -0400

    Delete now-unused fixture.

--- perma_web/conftest.py
+++ perma_web/conftest.py
@@ -877,19 +877,6 @@ def user_with_on_hold_subscription(mocker, link_user, on_hold_monthly_subscripti
 
 
 @pytest.fixture
-def user_with_requested_cancellation(mocker, link_user, cancellation_requested_subscription, no_purchase_history):
-    get_subscription = mocker.patch('perma.models.LinkUser.get_subscription', autospec=True)
-    get_purchase_history = mocker.patch('perma.models.LinkUser.get_purchase_history', autospec=True)
-
-    get_subscription.return_value = cancellation_requested_subscription
-    get_purchase_history.return_value = no_purchase_history
-
-    yield link_user
-
-    get_subscription.assert_called_once_with(link_user)
-
-
-@pytest.fixture
 def registrar_user_from_nonpaying_registrar(mocker, registrar_user, no_purchase_history):
     get_subscription = mocker.patch('perma.models.LinkUser.get_subscription', autospec=True)
     get_purchase_history = mocker.patch('perma.models.LinkUser.get_purchase_history', autospec=True)
